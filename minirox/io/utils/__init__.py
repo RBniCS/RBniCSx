edfe457dc54fcdde5f44c81f3592c26138e64718
Francesco Ballarin <francesco.ballarin@unicatt.it>
2021-12-28 15:09:18 +0100

    Import plot_mesh_tags as well

--- minirox/io/utils/__init__.py
+++ minirox/io/utils/__init__.py
@@ -4,11 +4,13 @@
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
-from minirox.io.utils.plotting import plot_mesh, plot_mesh_entities, plot_scalar_field, plot_vector_field
+from minirox.io.utils.plotting import (
+    plot_mesh, plot_mesh_entities, plot_mesh_tags, plot_scalar_field, plot_vector_field)
 
 __all__ = [
     "plot_mesh",
     "plot_mesh_entities",
+    "plot_mesh_tags",
     "plot_scalar_field",
     "plot_vector_field"
 ]
