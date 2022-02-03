4abf3d38f505c330d82b985d04dc0025422c9e4b
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-02-03 12:23:03 +0100

    Add option to skip backends tests in unit tests

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -11,9 +11,12 @@ This file assigns pytest hooks and declares common fixtures used across several 
 
 import typing
 
+import _pytest.config
+import _pytest.main
 import nbvalx.pytest_hooks_unit_tests
 import numpy as np
 import petsc4py
+import py
 import pytest
 import scipy.sparse
 
@@ -26,6 +29,23 @@ def to_dense_matrix() -> typing.Callable:
         ai, aj, av = mat.getValuesCSR()
         return scipy.sparse.csr_matrix((av, aj, ai), shape=(mat.getLocalSize()[0], mat.getSize()[1])).toarray()
     return _
+
+
+def pytest_addoption(parser: _pytest.main.Parser) -> None:
+    """Add an option to skip testing backends."""
+    parser.addoption("--skip-backends", action="store_true", help="Skip tests which require backends to be installed")
+
+
+def pytest_ignore_collect(path: py.path.local, config: _pytest.config.Config) -> bool:
+    """Honor the --skip-backends option to skip tests which require backends to be installed."""
+    skip_backends = config.option.skip_backends
+    if skip_backends:
+        if any([blacklist in str(path) for blacklist in ["tests/unit/backends/", "tests/unit/cpp/"]]):
+            return True
+        else:
+            return False
+    else:
+        return False
 
 
 pytest_runtest_setup = nbvalx.pytest_hooks_unit_tests.runtest_setup
