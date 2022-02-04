3e78e83592096f9ddcdbdb29a2caed424cb51b4d
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-02-04 15:30:56 +0100

    Simplify main init file, as slepc4py is a required dependency

--- rbnicsx/__init__.py
+++ rbnicsx/__init__.py
@@ -5,20 +5,14 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 """RBniCSx main module."""
 
-# Simplify import of mpi4py.MPI and petsc4py.PETSc in internal modules (and slepc4py.SLEPc
-# in tutorials, if available) by importing them here once and for all. Internal modules will
-# now only need to import the main packages mpi4py and petsc4py (and slepc4py, if available).
+# Simplify import of mpi4py.MPI, petsc4py.PETSc and slepc4py.SLEPc in internal modules by importing them here
+# once and for all. Internal modules will now only need to import the main packages mpi4py, petsc4py and slepc4py.
 import mpi4py
 import mpi4py.MPI  # noqa: F401
 import petsc4py
 import petsc4py.PETSc  # noqa: F401
-
-try:
-    import slepc4py
-except ImportError:  # pragma: no cover
-    pass
-else:
-    import slepc4py.SLEPc  # noqa: F401
+import slepc4py
+import slepc4py.SLEPc  # noqa: F401
 
 
 # We require a very small subset of the numpy typing library, namely NDArray. To avoid enforcing
@@ -43,8 +37,4 @@ del typing
 del mpi4py
 del numpy
 del petsc4py
-
-try:
-    del slepc4py
-except NameError:  # pragma: no cover
-    pass
+del slepc4py
