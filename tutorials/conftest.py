ee61c87b9b20db0703fd3e3e0df6b06c8afc04d5
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-01-04 10:03:30 +0100

    Simplify CI workflow file by moving tutorial skip inside conftest.py

--- tutorials/conftest.py
+++ tutorials/conftest.py
@@ -6,10 +6,18 @@
 """pytest configuration file for tutorials tests."""
 
 import nbvalx.pytest_hooks_notebooks
+import pytest
 
 pytest_addoption = nbvalx.pytest_hooks_notebooks.addoption
 pytest_sessionstart = nbvalx.pytest_hooks_notebooks.sessionstart
 pytest_collect_file = nbvalx.pytest_hooks_notebooks.collect_file
-pytest_runtest_setup = nbvalx.pytest_hooks_notebooks.runtest_setup
 pytest_runtest_makereport = nbvalx.pytest_hooks_notebooks.runtest_makereport
 pytest_runtest_teardown = nbvalx.pytest_hooks_notebooks.runtest_teardown
+
+
+def pytest_runtest_setup(item: nbvalx.pytest_hooks_notebooks.IPyNbFile) -> None:
+    """Skip tests if dolfinx is not available."""
+    # Do the setup as in nbvalx
+    nbvalx.pytest_hooks_notebooks.runtest_setup(item)
+    # Check dolfinx availability
+    pytest.importorskip("dolfinx")
